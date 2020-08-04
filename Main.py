b38a4cc969afa25fcd5910b6500e488d64d0bf44
FilipTrocin <mxtrocinski@gmail.com>
2020-08-04 12:40:51 +0200

    set main window not-resizable

--- Main.py
+++ Main.py
@@ -1,6 +1,8 @@
+from kivy.config import Config
+Config.set('graphics', 'resizable', False)
 from kivy.core.window import Window
 import UI
 
 Window.size = (600, 150)
 
-UI.MyApp().run()
+UI.AmazonPriceTrackerApp().run()
