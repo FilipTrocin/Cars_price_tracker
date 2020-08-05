ab19e105611a7f3dc69d75a73ae41b00156a1a38
FilipTrocin <mxtrocinski@gmail.com>
2020-08-05 08:55:56 +0200

    deleted on_enter() method, created popup window

--- UI.py
+++ UI.py
@@ -4,21 +4,21 @@ from kivy.app import App
 from kivy.app import Widget
 from kivy.core.text import LabelBase
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.textinput import TextInput
-from kivy.graphics import Color, Rectangle
-
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.popup import Popup
 
 kivy.require('1.11.1')
-
-
-def on_enter(value):
-    lista = []
-    lista.append(value)
-
 
 # Font for TextInput
 LabelBase.register('Arial_Rounded_Bold', fn_regular=os.path.join(os.path.dirname(__file__),
                                                                  'Font/Arial_Rounded_Bold.ttf'))
+
+
+class PopupWindow(FloatLayout):
+    @staticmethod
+    def popup_show():
+        pop = Popup(title="Analysis", content=PopupWindow, size_hint=(None, None), size=(500, 500))
+        pop.open()
 
 
 class CanvasWidget(BoxLayout):
