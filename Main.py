99549bf6b389c3aab8dc494a8638eed3746ede1c
FilipTrocin <mxtrocinski@gmail.com>
2020-08-22 13:20:22 +0200

    deleted unused import statement

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 from kivy.properties import get_color_from_hex
 from kivy.core.window import Window
 import UI
+import Database
 
 Window.size = (800, 390)
 Window.clearcolor = get_color_from_hex('#F9E29C')
