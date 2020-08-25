d2125933e20a5365ce05f72580fa74b371d88506
FilipTrocin <mxtrocinski@gmail.com>
2020-08-25 08:30:19 +0200

    created create_entry_receiver method

--- Database.py
+++ Database.py
@@ -1,8 +1,7 @@
 import pymongo
 import UI
-
-# Connection to the database
-from bson import ObjectId
+from multiprocessing import Pipe, Process
+import Parser
 
 
 def establish_connection():
@@ -18,6 +17,17 @@ def establish_connection():
     # new = collection.insert_many(post)
 
 
-# Temporary function to checking is data from UI.global_list is transfered to Database.py
+# Temporary function to checking is data from UI.global_list is transferred to Database.py
 def print_grabber():
     print('Hello Database!: ', UI.user_search_list)
+
+
+def create_entry_receiver():
+    """
+    :return: function receiving results of create_entry_sender method from Parser.py, initialising process with that
+    method
+    """
+    parent_conn, child_conn = Pipe()
+    p = Process(target=Parser.create_entry_sender, args=(child_conn,))
+    p.start()
+    print(parent_conn.recv())
