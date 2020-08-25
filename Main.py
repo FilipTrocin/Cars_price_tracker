58472e3b4bb21c9cd0c6fb77026b10a4fb7a57a9
FilipTrocin <mxtrocinski@gmail.com>
2020-08-25 08:29:36 +0200

    deleted import Database statement

--- Main.py
+++ Main.py
@@ -1,8 +1,8 @@
 from kivy.properties import get_color_from_hex
 from kivy.core.window import Window
 import UI
-import Database
 
 Window.size = (800, 390)
 Window.clearcolor = get_color_from_hex('#F9E29C')
+
 UI.PriceTrackerUIApp().run()
