8389041d0f658794644c0bb7edc7ce5b510ba4b2
FilipTrocin <mxtrocinski@gmail.com>
2020-08-25 12:26:43 +0200

    transferred import Parser statement to the create_entry_receiver method, modified docstrings

--- Database.py
+++ Database.py
@@ -1,7 +1,6 @@
 import pymongo
 import UI
 from multiprocessing import Pipe, Process
-import Parser
 
 
 def establish_connection():
@@ -19,15 +18,21 @@ def establish_connection():
 
 # Temporary function to checking is data from UI.global_list is transferred to Database.py
 def print_grabber():
+    """
+    ULTIMATELY - queering by this data from database
+    :return:
+    """
     print('Hello Database!: ', UI.user_search_list)
 
 
 def create_entry_receiver():
     """
     :return: function receiving results of create_entry_sender method from Parser.py, initialising process with that
+    ULTIMATELY - imputing this data to database
     method
     """
+    import Parser
     parent_conn, child_conn = Pipe()
     p = Process(target=Parser.create_entry_sender, args=(child_conn,))
     p.start()
-    print(parent_conn.recv())
+    print(parent_conn.recv())  # [{'MAKE': 'ford', 'MODEL': 'mondeo', 'MILEAGE': 178909, 'YEAR': 2007, ...
