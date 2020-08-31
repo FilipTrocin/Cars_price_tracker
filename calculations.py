454386dd418a7921750dd51ae337d5b8ffb61c8b
FilipTrocin <mxtrocinski@gmail.com>
2020-08-31 15:02:53 +0200

    renamed "prices", "objects", "temporary" variables to be more meaningful

--- calculations.py
+++ calculations.py
@@ -11,28 +11,28 @@ def average():
     Function counting an average price of that car with that specification user specified in UI
     :return:
     """
-    prices = []
-    objects = []
+    daily_prices = []
+    cars = []
     for x, y in enumerate(dates):
-        objects.extend(connection.find(
+        cars.extend(connection.find(
             {"MAKE": user_input[0], "MODEL": user_input[1], "YEAR": int(user_input[2]), "ENGINE_TYPE": user_input[3],
              "FIRST_SEARCH": dates[x]}))
 
     for x in dates:
-        temporary = []
-        for y in objects:
+        temp = []
+        for y in cars:
             if y['FIRST_SEARCH'] == x:
-                temporary.append(y['PRICE'])
+                temp.append(y['PRICE'])
                 # print('DAY: ', x, 'PRICE: ', y['PRICE'])
-        prices.append(temporary)
-    # print(prices, '\n')
+        daily_prices.append(temp)
+    # print(daily_prices, '\n')
     count = 0
-    for x in prices:
+    for x in daily_prices:
         avg = sum(x)/len(x)
         print(f'On day {dates[count]} car with that specification cost on average {round(avg, 2)}PLN')
         count += 1
 
-    flatten = [item for items in prices for item in items]
+    flatten = [item for items in daily_prices for item in items]
     overall_avg = sum(flatten)/len(flatten)
     print(f"Based on the past and current data the average price for car with that specification is: {round(overall_avg, 2)}PLN")
 
