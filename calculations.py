d7f4010f07c2d4d430ef80cfe986b24cbe50999a
FilipTrocin <mxtrocinski@gmail.com>
2020-08-31 14:55:27 +0200

    added counting average price of the car for every day available in database feature, simplify code for counting overall average price

--- calculations.py
+++ calculations.py
@@ -17,9 +17,22 @@ def average():
         objects.extend(connection.find(
             {"MAKE": user_input[0], "MODEL": user_input[1], "YEAR": int(user_input[2]), "ENGINE_TYPE": user_input[3],
              "FIRST_SEARCH": dates[x]}))
-    for item in objects:
-        prices.append(item['PRICE'])
 
-    avg = sum(prices)/len(prices)
-    print(f"Average price of that car with that specification is: {round(avg, 2)}PLN")
+    for x in dates:
+        temporary = []
+        for y in objects:
+            if y['FIRST_SEARCH'] == x:
+                temporary.append(y['PRICE'])
+                # print('DAY: ', x, 'PRICE: ', y['PRICE'])
+        prices.append(temporary)
+    # print(prices, '\n')
+    count = 0
+    for x in prices:
+        avg = sum(x)/len(x)
+        print(f'On day {dates[count]} car with that specification cost on average {round(avg, 2)}PLN')
+        count += 1
+
+    flatten = [item for items in prices for item in items]
+    overall_avg = sum(flatten)/len(flatten)
+    print(f"Based on the past and current data the average price for car with that specification is: {round(overall_avg, 2)}PLN")
 
