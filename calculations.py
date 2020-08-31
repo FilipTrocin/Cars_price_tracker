027a0ace2e24ba8048c7badaa57ac16f920d95df
FilipTrocin <mxtrocinski@gmail.com>
2020-08-31 13:04:49 +0200

    implemented an average method

--- calculations.py
+++ calculations.py
@@ -6,5 +6,20 @@ connection = establish_connection()
 dates = connection.distinct('FIRST_SEARCH')
 
 
+def average():
+    """
+    Function counting an average price of that car with that specification user specified in UI
+    :return:
+    """
+    prices = []
+    objects = []
+    for x, y in enumerate(dates):
+        objects.extend(connection.find(
+            {"MAKE": user_input[0], "MODEL": user_input[1], "YEAR": int(user_input[2]), "ENGINE_TYPE": user_input[3],
+             "FIRST_SEARCH": dates[x]}))
+    for item in objects:
+        prices.append(item['PRICE'])
 
+    avg = sum(prices)/len(prices)
+    print(f"Average price of that car with that specification is: {round(avg, 2)}PLN")
 
