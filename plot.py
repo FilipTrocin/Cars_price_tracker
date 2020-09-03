3db88f23acb15db9fdc8e305183a43661fc28532
FilipTrocin <mxtrocinski@gmail.com>
2020-09-03 09:06:27 +0200

    added string formatter (formatting values from yaxis), modified colour parameter of the bars, changed style of the plot

--- plot.py
+++ plot.py
@@ -1,10 +1,16 @@
 import matplotlib.pyplot as plt
 from calculations import dates_present, avg_daily_price
+from matplotlib.ticker import FormatStrFormatter
 
 
 def plot_bar_show():
-    plt.style.use('classic')
-    plt.bar(dates_present, avg_daily_price)
+    plt.style.use('ggplot')
+    fig, ax = plt.subplots()
+
+    formatter = FormatStrFormatter('%1.2f PLN')
+    ax.yaxis.set_major_formatter(formatter)
+
+    plt.bar(dates_present, avg_daily_price, color="orangered")
     plt.xlabel('DATE')
     plt.ylabel('PRICE')
     plt.show()
