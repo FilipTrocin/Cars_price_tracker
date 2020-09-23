b7246b6eaead380bc87cef47a930cd74d709ed9a
FilipTrocin <mxtrocinski@gmail.com>
2020-09-23 12:30:43 +0200

    added title to "plot_bar_show", created "weekly_bar_show" plotting a graph based on the average prices during the week

--- plot.py
+++ plot.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-from calculations import dates_present, avg_daily_price
+from calculations import dates_present, boundaries, avg_daily_price, avg_weekly_price
 from matplotlib.ticker import FormatStrFormatter
 
 
@@ -11,6 +11,21 @@ def plot_bar_show():
     ax.yaxis.set_major_formatter(formatter)
 
     plt.bar(dates_present, avg_daily_price, color="orangered")
+    plt.title('Daily Prices')
+    plt.xlabel('DATE')
+    plt.ylabel('PRICE')
+    plt.show()
+
+
+def weekly_bar_show():
+    plt.style.use('ggplot')
+    fig, ax = plt.subplots()
+
+    formatter = FormatStrFormatter('%1.2f PLN')
+    ax.yaxis.set_major_formatter(formatter)
+
+    plt.bar(boundaries, avg_weekly_price, color="turquoise")  # PRABABLE ISSUE WITH BOUNDARIES FORMAT AS IT IS PROVIDED AS A LIST
+    plt.title('Weekly Prices')
     plt.xlabel('DATE')
     plt.ylabel('PRICE')
     plt.show()
