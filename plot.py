54e6f84f81cfa856e686960867eccbd65cb20d36
FilipTrocin <mxtrocinski@gmail.com>
2020-10-02 12:18:22 +0200

    changed default size of `weekly graph`

--- plot.py
+++ plot.py
@@ -16,7 +16,7 @@ def plot_daily_graph():
 
 def plot_weekly_graph():
     plt.style.use('ggplot')
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(10, 4))
 
     formatter = FormatStrFormatter('%1.2f PLN')
     ax.yaxis.set_major_formatter(formatter)
