84ef4f18b12426e5e3a554920d032b9375ec6fba
FilipTrocin <mxtrocinski@gmail.com>
2020-10-03 14:41:36 +0200

    changed default size of window

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from kivy.properties import get_color_from_hex
 from kivy.core.window import Window
 import UI
 
-Window.size = (886, 499)
+Window.size = (885, 477)
 Window.clearcolor = get_color_from_hex('#F9E29C')
 
 UI.PriceTrackerUIApp().run()
