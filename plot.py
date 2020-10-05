ba37ca66c410ac64fd3555a5405d36451b04ca58
FilipTrocin <mxtrocinski@gmail.com>
2020-10-05 08:43:25 +0200

    declared `add_ten_dates` method

--- plot.py
+++ plot.py
@@ -3,6 +3,30 @@ from calculations import dates_present, boundaries, avg_daily_price, avg_weekly_
 from matplotlib.ticker import FormatStrFormatter
 
 
+def add_ten_dates(dates):
+    """
+    :param dates: list of all dates to be segregated
+    :return: list of lists of 10 dates inside each internal one or the remaining dates if there was no 10
+    """
+    ten_dates = []
+    count = 0
+    temp = []
+    for date in dates:
+        if count < 10:
+            temp.append(date)
+            count += 1
+        else:
+            cp = temp.copy()
+            ten_dates.append(cp)
+            temp.clear()
+            count = 0
+        if date is dates[-1]:
+            cp = temp.copy()
+            ten_dates.append(cp)
+            temp.clear()
+    return ten_dates
+
+
 def plot_daily_graph():
     plt.style.use('ggplot')
     fig, ax = plt.subplots(figsize=(10, 4))
